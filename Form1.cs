a91cc5adb38067057cdf3eb4e88bde4c9acbb89b
mamajonoff <newmamajonoff@gmail.com>
2022-04-22 17:58:28 +0500

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Music_Player
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] paths, files;
+
+        private void track_list_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            player.URL = paths[track_list.SelectedIndex];
+            player.Ctlcontrols.play();
+        }
+
+        private void guna2CircleButton1_Click(object sender, EventArgs e)
+        {
+            player.Ctlcontrols.pause();
+            button2.BringToFront();
+        }
+
+        private void guna2CircleButton2_Click(object sender, EventArgs e)
+        {
+            player.Ctlcontrols.play();
+            button1.BringToFront();
+        }
+
+        private void guna2CircleButton3_Click(object sender, EventArgs e)
+        {
+            if(track_list.SelectedIndex<track_list.Items.Count-1)
+            {
+                track_list.SelectedIndex = track_list.SelectedIndex + 1;
+            }
+        }
+
+        private void guna2CircleButton4_Click(object sender, EventArgs e)
+        {
+            if(track_list.SelectedIndex>0)
+            {
+                track_list.SelectedIndex = track_list.SelectedIndex - 1;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (player.playState == WMPLib.WMPPlayState.wmppsPlaying)
+            {
+                p_bar.Maximum = (int)player.Ctlcontrols.currentItem.duration;
+                p_bar.Value = (int)player.Ctlcontrols.currentPosition;
+            }
+
+            try
+            {
+                label1.Text = player.Ctlcontrols.currentPositionString;
+                label2.Text = player.Ctlcontrols.currentItem.durationString.ToString();
+            }
+
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void guna2CircleButton5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                files = ofd.FileNames;
+                paths = ofd.FileNames;
+                for(int i = 0; i < files.Length; i++)
+                {
+                    track_list.Items.Add(files[i]);
+                }
+            }
+        }
+    }
+}
